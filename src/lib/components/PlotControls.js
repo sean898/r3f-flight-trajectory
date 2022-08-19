99eafe4165160db0462e9754db08094992d1354f
Sean Reedy <15895395+sean898@users.noreply.github.com>
2022-08-19 11:19:47 -0400

    reset and snap controls

---
+++ src/lib/components/PlotControls.js
@@ -0,0 +1,61 @@
+import {Html} from '@react-three/drei';
+import {useThree} from '@react-three/fiber';
+import {Vector3} from 'three';
+import {initialCameraPosition} from './FlightPath.react';
+
+function getCoordinates(dataObj) {
+    const {x, y, z} = dataObj;
+    return new Vector3(x, y, z);
+}
+
+const cameraOffset = new Vector3(0, 50, 110);
+const normal = new Vector3(0, 1, 0);
+const origin = new Vector3(0, 0, 0);
+
+export function PlotControls({
+    currentData,
+    incrementIndex,
+    controlsRef,
+    ...props
+}) {
+    // useFrame(() => incrementIndex());
+    const camera = useThree((state) => state.camera);
+    const setCameraPosition = (position) => {
+        camera.position.set(...position);
+    };
+
+    const resetControls = () => {
+        controlsRef.current.update();
+    };
+
+    function setCamera(position, target) {
+        setCameraPosition(position);
+        camera.updateProjectionMatrix();
+        controlsRef.current.target.set(target.x, target.y, target.z);
+        controlsRef.current.update();
+    }
+
+    const resetPlot = () => {
+        setCamera(initialCameraPosition, origin);
+    };
+
+    const snapToAircraft = () => {
+        const aircraftPosition = getCoordinates(currentData);
+        const cameraPosition = aircraftPosition.clone().add(cameraOffset);
+        console.log(aircraftPosition, cameraPosition);
+        setCamera(cameraPosition, aircraftPosition);
+    };
+
+    return (
+        <Html
+            wrapperClass="plot-controls-wrapper"
+            className="plot-controls"
+            transform={false}
+            center={false}
+        >
+            <button onClick={resetPlot}>Reset</button>
+            <button onClick={snapToAircraft}>Snap</button>
+            Controls!
+        </Html>
+    );
+}
