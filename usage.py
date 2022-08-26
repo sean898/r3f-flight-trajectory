8f216af90b574bc9860a23c57164112ee22119e1
Sean Reedy <15895395+sean898@users.noreply.github.com>
2022-08-26 15:57:20 -0400

    working app

--- usage.py
+++ usage.py
@@ -1,27 +1,67 @@
+from dash import dcc
 import flight_path
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import pandas as pd
 
 app = dash.Dash(__name__)
 
-data = pd.read_csv('src/demo/data/test.csv', nrows=700).to_dict(orient='records')
-print(data[0])
+flight_data = pd.read_csv('src/demo/data/test.csv', nrows=700)
 
-app.layout = html.Div([
+app.layout = html.Div(style={'display': 'flex', 'height': '100vh'}, children=[
     flight_path.FlightPath(
-        id='input',
-        data=data,
+        id='path',
         segmentInfo=[],
         modelFile=app.get_asset_url('F-16.glb'),
         counter=0
     ),
-    html.Div(id='output')
+    html.Button(id='play-toggle', children='Play/pause'),
+    dcc.Slider(id='time-slider', marks=None, min=0, max=len(flight_data), step=1, updatemode='drag', tooltip={'placement': 'bottom', 'always_visible': True}),
+    html.Div(id='output'),
+    dcc.Store(id='flight-data', data=flight_data.to_dict(orient='records')),
+    dcc.Interval(id='playback-interval', interval=100),
 ])
 
+
+app.clientside_callback(
+    """function(data) {
+        return data;
+    }""",
+    Output('path', 'data'),
+    Input('flight-data', 'data')
+)
+
+
+app.clientside_callback(
+    """function(toggle, state) {
+        return !state;
+    }""",
+    Output('playback-interval', 'disabled'),
+    Input('play-toggle', 'n_clicks'),
+    State('playback-interval', 'disabled'),
+)
+
+app.clientside_callback(
+    """function(interval, state) {
+        return state + 1;
+    }""",
+    Output('time-slider', 'value'),
+    Input('playback-interval', 'n_intervals'),
+    State('time-slider', 'value'),
+)
+
+
+app.clientside_callback(
+    """function(time_value) {
+        return time_value;
+    }""",
+    Output('path','counter'),
+    Input('time-slider', 'value'),
+)
 
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
