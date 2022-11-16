962bb09492aa4eee760fd34f9bbed0e22a70694a
Sean Reedy <15895395+sean898@users.noreply.github.com>
2022-11-16 12:06:32 -0500

    Add Notes.js

---
+++ src/lib/components/Notes.js
@@ -0,0 +1,24 @@
+import {Html} from '@react-three/drei';
+import PropTypes from 'prop-types';
+
+function Notes({notes}) {
+    if (notes == null || notes.length == 0) return <></>;
+    return notes.map(({x, y, z, text}, i) => (
+        <mesh key={`notes=${i}`} position={[x, y, z]}>
+            <Html wrapperClass="hover-info-wrapper">
+                <div class="note-annotation">{text}</div>
+            </Html>
+        </mesh>
+    ));
+}
+
+Notes.propTypes = {
+    /** Note array */
+    notes: PropTypes.array,
+};
+
+Notes.defaultProps = {
+    notes: [],
+};
+
+export {Notes};
